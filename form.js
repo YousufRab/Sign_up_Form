a6030d98fe8314d27002aa496dddf136812cd51b
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-09-03 13:37:51 -0700

    Create submit button variable

--- form.js
+++ form.js
@@ -1,4 +1,9 @@
-function validatePass {
+function validatePass() {
     var pw1 = document.getElementById('pwd');
     var pw2 = document.getElementById('confirmPwd');
+    if (pw1 != pw2) {
+        alert("Passwords did not match");
+    }
 }
+
+submitBtn = document.getElementById('subBtn');
