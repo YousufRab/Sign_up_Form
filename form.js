e58abc0573df6355eb1940e0ca40d5e90a32fa96
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-09-05 14:03:51 -0700

    Correct addeventlistener typo

--- form.js
+++ form.js
@@ -2,18 +2,16 @@ function validatePass() {
     var pw1 = document.getElementById('pwd');
     var pw2 = document.getElementById('confirmPwd');
     if (pw1 != pw2) {
-        alert("Passwords did not match");
+        alert("Passwords did not match. Please provide matching passwords");
         return false;
     } else {
         return true;
     }
 }
 
-document.addEventlistener('BeforeFormSubmit', function(event) {
-    var pw1 = document.getElementById('pwd');
-    var pw2 = document.getElementById('confirmPwd');
-    if (pw1 != pw2) {
-        event.preventDefault();
-        alert("Passwords did not match")
-    }
+const submit = document.getElementById('subBtn');
+submit.addEventListener('click', (event) => {
+    if (validatePass() == false) {
+        event.preventDefault()
+    } 
 })
