2f531d2ce5562d1cb8d91b83e02fc1826759ad88
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-09-27 11:53:18 -0700

    Add .value to pw1 and pw2 element selectors in validatePass function to correctly verify content entered in password and confirm password fields are identical

--- form.js
+++ form.js
@@ -1,6 +1,6 @@
 function validatePass() {
-    var pw1 = document.getElementById('pwd');
-    var pw2 = document.getElementById('confirmPwd');
+    var pw1 = document.getElementById('pwd').value;
+    var pw2 = document.getElementById('confirmPwd').value;
     if (pw1 != pw2) {
         alert("Passwords did not match. Please provide matching passwords");
         return false;
